2594e259be4216b6cbb40370b4b83bdd9a2c98bd
Flavio Pegoraro <flaviopegoraro2@gmail.com>
2026-01-29 21:11:38 +1300

    build2

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
-  },
   base: "/",
 });
